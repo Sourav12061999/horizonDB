fcb8fd50bde08cb089fd32a3a558e7324775a06f
Sourav12061999 <sdunity12061999@gmail.com>
2023-12-20 09:54:35 +0530

    Database creation handler ready

--- src/Storage Engine/Database.ts
+++ src/Storage Engine/Database.ts
@@ -1,5 +1,5 @@
 import { ErrorHandler } from "../Utils";
-import { mkdir, } from "fs/promises";
+import { mkdir, readdir } from "fs/promises";
 import { existsSync } from "fs";
 import { join } from "path";
 import Table from "./Tables";
@@ -13,23 +13,27 @@ export default class Database extends ErrorHandler {
 
     }
 
-    private craeteNewDatabaseHandler() {
-        // Here the Error handler will take care of any errors that happens while creating a database connection
-        this.errorHandler(this.dbCreationHandler);
+    static async craeteNewDatabaseHandler(dbName: string) {
+        await Database.dbCreationHandler(dbName);
+        return new Database(dbName, []);
     }
-    private async dbCreationHandler() {
+    private static async dbCreationHandler(dbName: string) {
         // This function is actally responsible for creating the database
-        const dbPath = join(__dirname, `Databases/${this.db}`);
+        const dbPath = join(__dirname, `Databases/${dbName}`);
         const isExist = existsSync(dbPath);
-        if (isExist) throw new Error(`Database "${this.db}" already exists`)
+        if (isExist) throw new Error(`Database "${dbName}" already exists`)
         await mkdir(dbPath);
     }
-
-    static createNewDatabase(dbPath: string) {
-
-    }
-
-    static loadAllDatabases() {
-
+    // Loading all the existing databases complete
+    static async loadAllDatabases() {
+        const Path = join(__dirname, `Databases`);
+        const Folders = await readdir(Path, { withFileTypes: true });
+        const databases = Folders
+            .filter(dirent => dirent.isDirectory())
+            .map(dirent => dirent.name);
+        return databases.map(async (database) => {
+            const tables = await Table.loadAllTables(join(__dirname, `Databases`, database));
+            return new Database(database, tables);
+        })
     }
 }
