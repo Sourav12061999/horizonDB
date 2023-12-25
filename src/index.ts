2f20376aed485afa2cb2b69507aa0b919b33e09f
Sourav12061999 <sdunity12061999@gmail.com>
2023-12-25 15:03:57 +0530

    Basic setup of database complete

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,53 @@
 
 import { StorageEngine } from "./Storage Engine";
+import parseSQL from "./Parser";
 class Main {
-
     storageEngine: StorageEngine | null = null;
     static instance: Main | null = null;
-    private constructor() {
-        Main.connect();
+    private constructor(afterSetupCallback: () => void) {
+        StorageEngine.connect().then((engine) => {
+            this.storageEngine = engine;
+            afterSetupCallback();
+         });
     }
 
-    static async connect() {
-        Main.instance = new Main();
+    static async connect(afterSetupCallback: () => void){
+        if (!Main.instance) {
+            Main.instance = new Main(afterSetupCallback);
+
+        }
+
+
+    }
+
+    async parser(query: string) {
+        const ast = parseSQL(query);
+        await this.methodCallHandler(ast)
+    }
+
+    private async methodCallHandler(ast: any) {
+
+        if (!this.storageEngine) {
+            throw new Error("StorageEngine setup is not complete yet."); 
+        }
+        switch (ast.type) {
+            case "create_database":
+                await this.storageEngine.createDatabase("");
+                return;
+            case "use_database":
+                this.storageEngine.useDatabase("");
+                return;
+            case "create_table":
+                await this.storageEngine.createtable("", {});
+                return;
+
+            case "update":
+                await this.storageEngine.updteTableRow("")
+                return;
+
+            case "delete":
+                await this.storageEngine.deleteTableRow("")
+                return;
+        }
     }
 }
